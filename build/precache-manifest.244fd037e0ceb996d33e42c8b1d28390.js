133a57b34ff9e10131f2912e10be3a5e8559714b
Ahmad <31131137+Ahmaddar18@users.noreply.github.com>
2020-05-12 11:03:59 +0200

    some miner fix

---
+++ build/precache-manifest.244fd037e0ceb996d33e42c8b1d28390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88fa21d87b567e3ba3fd138a82e6976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfc11d9a0e834f0fe1b9",
+    "url": "/static/css/2.58023d38.chunk.css"
+  },
+  {
+    "revision": "dfda3dd502762db7476e",
+    "url": "/static/css/main.c59dcbed.chunk.css"
+  },
+  {
+    "revision": "dfc11d9a0e834f0fe1b9",
+    "url": "/static/js/2.4f9d4ecc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.4f9d4ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfda3dd502762db7476e",
+    "url": "/static/js/main.cbe0c08d.chunk.js"
+  },
+  {
+    "revision": "ff0a20cdd9857ca5538b",
+    "url": "/static/js/runtime-main.d9ca6474.js"
+  }
+]);
